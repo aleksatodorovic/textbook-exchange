916c69fb84e8b59d0c91048df891a3b20e7af7d9
aleksatodorovic <atodorov@tulane.edu>
2018-10-29 15:02:21 +0000

    pre-pull commits

--- db/migrate/20180926202114_create_books.rb
+++ db/migrate/20180926202114_create_books.rb
@@ -1,4 +1,4 @@
-class CreateBooks < ActiveRecord::Migration
+class CreateBooks < ActiveRecord::Migration[5.1]
   def change
     create_table :books do |t|
       t.text :title
--- db/migrate/20181013154504_create_users.rb
+++ db/migrate/20181013154504_create_users.rb
@@ -1,5 +1,6 @@
-class CreateUsers < ActiveRecord::Migration
+class CreateUsers < ActiveRecord::Migration [5.1]
   def change
+    add_column :books
     create_table :users do |t|
       t.text :name
       t.text :phone
