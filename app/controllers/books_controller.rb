8b907b49f837310cc1cef0a98b3c9836a03e9d5d
gregm700 <gregm700@verizon.net>
2018-11-15 17:46:02 +0000

    Full google user functionality

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,13 +30,14 @@ class BooksController < ApplicationController
     def destroy
         Book.find(params[:id]).destroy
         flash[:success] = "Book deleted"
-        redirect_to '/home'
+        redirect_to '/index'
     end
 
     def create 
         @books = Book.new(m_params)
+        @books.user = current_user
         if @books.save 
-            redirect_to '/home'
+            redirect_to '/index'
         else 
             flash.now[:notice] = "Error adding book"
             redirect_to '/books'
@@ -63,7 +64,7 @@ class BooksController < ApplicationController
     end
     private 
     def m_params
-        params.require(:book).permit(:title, :isbn, :user_id)
+        params.require(:book).permit(:title, :isbn)
     end
 
 
