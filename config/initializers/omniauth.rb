6538520cf34366d952ad4d1ccec0527160be81e1
Brianna Tucker <btucker2@tulane.edu>
2018-12-04 18:13:10 +0000

    fixed validation on book entry

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '39952447542-p0e5vq98nh7cvq0hajs25i32q1o6vvki.apps.googleusercontent.com', 'CeLlX040l2Q05k_8P9mOKG2N', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, '476593344759-2ae2akv33mdchoqm4jhgcksvf1b62a02.apps.googleusercontent.com', 'b4Carq-zMr9mNSVT8YoNPuxB', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
