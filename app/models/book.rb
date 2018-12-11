a901f86ab88ee77888af65a02f6430066ddeafea
shees-a <sahmed8@tulane.edu>
2018-12-11 03:59:15 +0000

    Users can now upload an image to their post

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,12 @@
 class Book < ActiveRecord::Base 
     belongs_to :user
     
-    mount_uploader :image, ImageUploader
+    
     validates :title,  presence: true
     validates :isbn,  presence: true, :numericality => {:only_integer => true}
     validates :price, presence: true
     validates_presence_of :user
+    mount_uploader :image, ImageUploader
     
     
     def self.search(terms)
