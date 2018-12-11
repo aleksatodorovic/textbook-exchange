abae4426230934fc6712ffc5bf1bfa48e405f2c9
aleksatodorovic <atodorov@tulane.edu>
2018-12-11 03:09:48 +0000

    sqlite3 and development log save

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,9 @@
 ActiveRecord::Schema.define(version: 20181013154504) do
 
   create_table "books", force: :cascade do |t|
-    t.text     "price"
     t.text     "title"
     t.text     "isbn"
+    t.text     "price"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
