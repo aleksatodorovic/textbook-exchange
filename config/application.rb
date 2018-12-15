ad1cf59fe4448cb900f0e93fa38c035904578ffd
aleksatodorovic <atodorov@tulane.edu>
2018-12-15 16:11:36 +0000

    Reconfigured production and application files for heroku

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,12 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+#Bundler.require(*Rails.groups)
+Bundler.require(:default, :assets, Rails.env)
+# Enable the asset pipeline
+config.assets.enabled = true
+# Version of your assets, change this if you want to expire all your assets
+config.assets.version = '1.0'
 
 module Workspace
   class Application < Rails::Application
